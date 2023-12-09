67971be67b1bbb108fa7181a32d259a45293f014
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-09 22:18:07 +0530

    adding comments

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,15 @@ import Footer from "./components/footer/Footer";
 const App = () => {
   return (
     <>
+      {/* Header component */}
       <Header />
+      {/* Navigation component */}
       <Nav />
+      {/* About component */}
       <About />
+      {/* Contact component */}
       <Contact />
+      {/* Footer component */}
       <Footer />
     </>
   );
