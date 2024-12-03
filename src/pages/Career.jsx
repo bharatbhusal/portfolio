c20054e85d7da1e17166caa1a61c1c7ab90471a9
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-12-03 09:22:31 +0530

    responsive slider

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,45 +1,32 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import Carousel from "../components/utils/Carousel";
 
-import { Pagination, Navigation } from "swiper/modules";
 import CareerCard from "../components/CareerCard";
 import careerData from "../data/careerData";
 
 const Career = () => {
+	const SLIDES = careerData.map((item, index) => {
+		return (
+			<CareerCard
+				key={index}
+				company={item.company}
+				role={item.role}
+				duration={item.duration}
+				address={item.address}
+				description={item.description}
+				achievements={item.achievements}
+				links={item.links}
+			/>
+		);
+	});
+
+	const OPTIONS = { loop: true };
 	return (
 		<div className="flex flex-col justify-around w-full h-[90vh] z-0">
 			<h1 className="text-4xl font-bold text-center">
 				Career
 			</h1>
 			<div className="flex justify-between w-full h-[90%]">
-				<Swiper
-					spaceBetween={2}
-					slidesPerView={1}
-					pagination={{
-						clickable: true,
-					}}
-					navigation={true}
-					modules={[Pagination, Navigation]}
-				>
-					{careerData.map((item, index) => (
-						<SwiperSlide
-							key={index}
-							className="px-2 flex flex-col justify-center items-center"
-						>
-							<CareerCard
-								company={item.company}
-								role={item.role}
-								duration={item.duration}
-								address={item.address}
-								description={item.description}
-								achievements={item.achievements}
-								links={item.links}
-							/>
-						</SwiperSlide>
-					))}
-				</Swiper>
+				<Carousel slides={SLIDES} options={OPTIONS} />
 			</div>
 		</div>
 	);
