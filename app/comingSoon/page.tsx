9599f7f9d9c58e7385d806f0880a5b72affbef72
bharatbhusal <bharatbhusal78@gmail.com>
2025-02-22 22:11:33 +0530

    remove page not found and center coming soon page:

--- app/comingSoon/page.tsx
+++ app/comingSoon/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/Button";
 const ComingSoon = () => {
 	const router = useRouter();
 	return (
-		<div className={`text-center`}>
+		<div className="flex flex-col items-center justify-center min-h-[90%] w-full">
 			<h1 className="text-6xl font-bold mb-4">Coming Soon</h1>
 			<p className="text-2xl text-gray-500 mb-8">
 				I am working hard to bring you something amazing on this
