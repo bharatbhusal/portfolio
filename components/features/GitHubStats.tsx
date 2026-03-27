f1878b6df1683c961eff63e3194dfad07d41b00c
bharatbhusal <bharatbhusal78@gmail.com>
2026-03-27 07:33:11 +0530

    fix: update GitHub Top Langs Card theme parameter to use dynamic theme

--- components/features/GitHubStats.tsx
+++ components/features/GitHubStats.tsx
@@ -30,7 +30,7 @@ const GitHubStats = ({ username }: GitHubStatsProps) => {
         {/* GitHub Top Langs Card */}
         <div className="flex justify-center">
           <Image
-            src={`https://github-readme-stats-fast.vercel.app/api/top-langs/?username=${username}&layout=compact&theme=radical&theme=${themeParam}&hide_border=true&background=00000000`}
+            src={`https://github-readme-stats-fast.vercel.app/api/top-langs/?username=${username}&layout=compact&theme=${themeParam}&hide_border=true`}
             alt="GitHub Top Langs"
             width={495}
             height={195}
